1f93dd0c2292b063d2df9c4412ba5591ee722808
21A91A05D6 <97495941+21A91A05D6@users.noreply.github.com>
2022-10-12 23:29:40 +0530

    import java.util.Scanner; class multiple{     public static void main(String[]args){         //int name1 = reader.readLine();         int a,b,c;         Scanner input= new Scanner(System.in);         a=input.nextInt();         b=input.nextInt();         c=a*b;         System.out.println(c);     } }

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class multiple{
+    public static void main(String[]args){
+        //int name1 = reader.readLine();
+        int a,b,c;
+        Scanner input= new Scanner(System.in);
+        a=input.nextInt();
+        b=input.nextInt();
+        c=a*b;
+        System.out.println(c);
+    }
+}
